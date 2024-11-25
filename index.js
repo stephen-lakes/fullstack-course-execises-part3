e1660efd62ada095d19f4b1de36e822e4834e762
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-25 02:31:54 +0100

    Add endpoint to delete person by ID with 204 status code
    - Created a DELETE route at /api/persons/:id to remove a person by their
      ID
      - Filters out the person with the specified ID from the persons array
      - Responds with a 204 No Content status code to indicate successful
        deletion
      - Update persons varaible declaration using let to allow update on it
        values within the delete endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -38,6 +38,12 @@ app.get("/api/persons/:id", (request, response) => {
     response.status(404).send("Person not Found");
   }
   response.send(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
 });
 
 app.get("/info", (request, response) => {
