ec94b86144cf1f4f465ee09a32f046478ecd882c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-25 01:20:54 +0100

    implement sever with express

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
-console.log("Server listening on port 3000");
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {});
+
+const PORT = 3000;
+app.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
