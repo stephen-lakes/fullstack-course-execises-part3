1289d6d728d74320828555e91c114320488ddf1b
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-06 00:22:15 +0100

    feat: add command-line argument handling and conditional logic for phonebook operations in MongoDB

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 const name = process.argv[3];
-const phone = process.argv[4];
+const number = process.argv[4];
 
 const url = `mongodb+srv://fullstack-phonebook-prod:${password}@phonebook-prod-cluster.aaquz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=phonebook-prod-cluster`;
 mongoose.set("strictQuery", false);
@@ -16,22 +16,25 @@ mongoose.connect(url);
 
 const phonebookSchema = mongoose.Schema({
   name: String,
-  phone: String,
+  number: String,
 });
 
 const Phonebook = mongoose.model("Phonebook", phonebookSchema);
 
-// const newContact = new Phonebook({
-//   name,
-//   phone: phone,
-// });
+if (!name && !number) {
+  Phonebook.find({}).then((result) => {
+    console.log("phonebooK:");
+    result.forEach((contact) => console.log(contact));
+    mongoose.connection.close();
+  });
+} else {
+  const newContact = new Phonebook({
+    name,
+    number,
+  });
 
-// newContact.save().then(contact => {
-//   console.log("DONE", contact);
-//   mongoose.connection.close();
-// });
-
-Phonebook.find({}).then((result) => {
-  result.forEach(contact => console.log(contact));
-  mongoose.connection.close();
-});
+  newContact.save().then((contact) => {
+    console.log(`$added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
