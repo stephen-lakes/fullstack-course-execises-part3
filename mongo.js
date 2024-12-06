7cb4eee35cad115f6d044aad609778ae63a581d6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-06 01:12:42 +0100

    feat: enhance phonebook functionality with dotenv, improved schema, and command-line args - Added dotenv for secure environment variable management - Improved MongoDB connection setup using MONGODB_URI from environment - Enhanced phonebook schema with proper field names - Implemented command-line argument handling for password, name, and number - Conditional logic to either list all contacts or add a new contact - Ensured proper error handling and graceful MongoDB connection closure

--- mongo.js
+++ mongo.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
@@ -9,7 +10,7 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
-const url = `mongodb+srv://fullstack-phonebook-prod:${password}@phonebook-prod-cluster.aaquz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=phonebook-prod-cluster`;
+const url = process.env.MONGODB_URI;
 mongoose.set("strictQuery", false);
 
 mongoose.connect(url);
