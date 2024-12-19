b86c6cc3e25a27871f2232605decb90d058b4283
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-19 05:32:33 +0100

    add schema validation

--- models/person.js
+++ models/person.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 const url = process.env.MONGODB_URI;
 
-console.log("connection to", url);
+console.log('connection to', url);
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+    console.log('error connecting to MongoDB:', error.message);
   });
 
 const validateNumber = (number) => {
   // check for correct format
-  const parts = number.split("-");
+  const parts = number.split('-');
   if (parts.length !== 2) return false;
 
   if (!/^\d{2,3}$/.test(parts[0]) || !/^\d+$/.test(parts[1])) return false;
@@ -35,14 +35,13 @@ const personSchema = mongoose.Schema({
     type: String,
     validate: {
       validator: validateNumber,
-      message: (props) =>
-        `${props.value} is not a valid phone number! It should be in the format XX-XXXXXXX or XXX-XXXXXXXX.`,
+      message: (props) => `${props.value} is not a valid phone number! It should be in the format XX-XXXXXXX or XXX-XXXXXXXX.`,
     },
-    required: [true, "User phone number required"],
+    required: [true, 'User phone number required'],
   },
 });
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
@@ -50,4 +49,4 @@ personSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema);
