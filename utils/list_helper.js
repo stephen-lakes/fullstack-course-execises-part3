f74c11180c80014f22b186b43e4fb73d5ccf6afa
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 04:43:38 +0100

    feat: Add blog utility functions
    - Implemented `dummy` function as a simple placeholder
    - Implemented `totalLikes` function to calculate total likes of all
      blogs
      - Implemented `favoriteBlog` function to find the blog with the most
        likes
        - Implemented `mostBlogs` function to find the author with the most
          blog posts
          - Implemented `mostLikes` function to find the author with the
    	most likes across all blog posts
    	- Exported all functions for external use

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,59 @@
 const dummy = (blogs) => {
-  return 1
+  return 1;
 };
 
-module.exports = {
-  dummy,
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return 0;
+
+  return blogs.reduce(
+    (fav, blog) =>
+      blog.likes > (fav?.likes || 0)
+        ? { title: blog.title, author: blog.author, likes: blog.likes }
+        : fav,
+    null
+  );
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return 0;
+
+  let authorBlogCount = {};
+  blogs.forEach((blog) => {
+    if (authorBlogCount[blog.author]) {
+      authorBlogCount[blog.author].count += 1;
+    } else {
+      authorBlogCount[blog.author] = { count: 1 };
+    }
+  });
+
+  let topAuthor = Object.keys(authorBlogCount).reduce((acc, b) =>
+    authorBlogCount[acc] > authorBlogCount[b] ? acc : b
+  );
+
+  return { author: topAuthor, blogs: authorBlogCount[topAuthor].count };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+
+  const authorLikesCount = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes;
+    return acc;
+  }, {});
+
+  let topAuthor = Object.keys(authorLikesCount).reduce((acc, item) => {
+    if (authorLikesCount[acc] > authorLikesCount[item]) return acc;
+    else return item;
+  });
+
+  return {
+    author: topAuthor,
+    likes: authorLikesCount[topAuthor],
+  };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
